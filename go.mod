9200bddc1f62fddc1c25b01850d3f68b95b8f673
João Alberto <hey@joaoalberto.dev>
2024-01-05 13:07:38 -0300

    feat: list and filter repository props

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/infinite-nil/blog-api
 go 1.21.5
 
 require (
+	github.com/google/go-github/v57 v57.0.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/labstack/echo/v4 v4.11.4 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
--- main.go
+++ main.go
@@ -1,16 +1,39 @@
 package main
 
 import (
+	"context"
 	"net/http"
 
+	"github.com/google/go-github/v57/github"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	app := echo.New()
+	client := github.NewClient(nil)
+	repoOptions := &github.RepositoryListByUserOptions{Type: "public"}
 
-	app.GET("/", func(context echo.Context) error {
-		return context.String(http.StatusOK, "OK")
+	app.GET("/", func(ctx echo.Context) error {
+		repos, _, err := client.Repositories.ListByUser(context.Background(), "infinite-nil", repoOptions)
+
+		if err != nil {
+			// TODO: Improve error handling
+			return echo.ErrInternalServerError
+		}
+
+		cleanRepos := make([]*github.Repository, len(repos))
+
+		for i := 0; i < len(repos); i++ {
+			cleanRepos[i] = &github.Repository{
+				Name: repos[i].Name,
+				HTMLURL: repos[i].HTMLURL,
+				Language: repos[i].Language,
+				Description: repos[i].Description,
+				Topics: repos[i].Topics,
+			}
+		}
+
+		return ctx.JSON(http.StatusOK, cleanRepos)
 	})
 
 	app.Logger.Fatal(app.Start(":10000"))
